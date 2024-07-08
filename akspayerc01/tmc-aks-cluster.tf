7e1828407d93c645fc4933a6f716c8f2e89801fb
Bob Bauer <rotbau@gmail.com>
2024-07-08 16:19:05 -0500

    updated variables

--- akspayerc01/tmc-aks-cluster.tf
+++ akspayerc01/tmc-aks-cluster.tf
@@ -1,12 +1,4 @@
 terraform {
-  required_providers {
-    tanzu-mission-control = {
-      source = "vmware/dev/tanzu-mission-control"
-    }
-  }
-}
-
-terraform {
   backend "local" {
     path = "./terraform.tfstate"
   }
