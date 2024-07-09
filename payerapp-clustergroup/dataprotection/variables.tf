a365b1ff2f13c1d256ef5d9ee79f427137c288a1
Bob Bauer <37818713+rotbau@users.noreply.github.com>
2024-07-09 09:49:52 -0500

    Delete payerapp-clustergroup/dataprotection directory

--- payerapp-clustergroup/dataprotection/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vmw_cloud_api_token" {
-  description = "api token for auth"
-  type        = string
-}
-
-variable "cluster_group" {
-  description = "clustergroup to enable data protection"
-  type        = string
-}
